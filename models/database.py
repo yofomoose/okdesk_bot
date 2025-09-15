788e074b0eebc39c598b896821bbe96c6309e2bc
YofoMoose <dove164101@gmail.com>
2025-09-15 14:05:39 +0600

    .

--- models/database.py
+++ models/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text, Boolean, BigInteger
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text, Boolean, BigInteger, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
